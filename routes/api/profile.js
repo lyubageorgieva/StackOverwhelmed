daaf38fe6c1d0899518df7c9698223783c8f1f58
NicholasWahome <87381233+NicholasWahome@users.noreply.github.com>
2021-10-17 20:08:52 -0400

    Revert "Update profile.js"
    This reverts commit 08a7113074bcd049236e3ba6ee42c21ffa57ffa1.

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,46 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const auth = require('../../middleware/auth');
-const {check, validationResult} = require('express validator - valida');
 
-const Profile = require('../../models/Profile');
-const User = require('../../models/User');
-
-// @route       GET api/profile/me
-// @description Get current users profile
-// @access      Private
-router.get('/me', (req, res) => {
-    try{
-    const profile = await Profile.findOne({user: req.user.id}).populate('user',
-    ['name', 'avatar']);
-
-    if(!profile){
-        return res.status(400).json({msg: 'There is no profile for this user'});
-    }
-    }
-    catch(err){
-    console.error(err.message);
-    res.status(500).send('Server Error');
-    }
-
-});
 // @route       GET api/profile
-// @description Create wew or Udpate user profile 
-// @access      Private
-router.post('/',[auth,
-    [
-    check('bio', 'Bio is required')
-    .not()
-    .isEmpty,
-]
-],
-    async(req, res) => {
-     const errors = validationResult(rep);
-     if(!errors.isEmpty()){
-         return res.status(400).json({erros: errors.array()})
-     }
-    }
-    );
+// @description Test route
+// @access      Public
+router.get('/', (req, res) => res.send('Profile route'));
 
 // export route
 module.exports = router;
