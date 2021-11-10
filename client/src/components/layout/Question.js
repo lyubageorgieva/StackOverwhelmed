c68d8f12cb7fa7165d48a667a4c375140702b53a
jexi <allan_lopez24@hotmail.ca>
2021-11-10 18:27:27 -0500

    Modified post page #86 #88 #93 #102 #105 #138 #139 #140
    - upvote/downvote/supervote icons modified
    - upvote/downvote counter separated
    - add answer/comment input boxes separated
    - added X button to delete post, comment, or answer

--- client/src/components/layout/Question.js
+++ client/src/components/layout/Question.js
@@ -7,21 +7,9 @@ export const Question = () => {
             <div className="row q1">
                 <form action="/action_page.php">
                     <label for="title">Title</label>
-                    <input type="text" className="title col_right" name="title" maxlength="280" placeholder="What's your question?"/><br/><br/>
-                    <textarea id="details" name="details" rows="10" cols="100"></textarea><br/><br/>
-                    <div className="row tags-box">
-                        <ul id="myUL">
-                            <input type="text" id="myInput" placeholder="Add a tag..." maxlength="16" />
-                        </ul>
-                    </div>
-                    
-                    <div id="myDIV" className="row">
-                        <span onclick="newElement()" className="addBtn">Add</span>
-                    </div>
-
-                    
-                    <label for="img">Select image:</label>
-                    <input type="file" id="img" name="img" accept="audio/*,video/*,image/"/>
+                    <input type="text" className="title col_right" name="title" maxlength="280" placeholder="What's your question?"/>
+                    <br/><br/>
+                    <textarea id="details" name="details" rows="10" cols="100"></textarea>
                     <br/><br/>
                     <input type="submit" value="Submit question"/>
                 </form>
