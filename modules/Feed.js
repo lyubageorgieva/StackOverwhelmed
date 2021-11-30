6a53babd53c8c4e5feb0dff21e0d557e9f99568e
SimplyRed900 <a_skoko@live.concordia.ca>
2021-11-30 16:51:18 -0500

    Fix for Required Title text bug
    Voting should work now without requirement  being added

--- modules/Feed.js
+++ modules/Feed.js
@@ -1,63 +1,70 @@
-const mongoose = require ('mongoose');
+const mongoose = require('mongoose');
 
 
 const FeedSchema = new mongoose.Schema({
 
 
 
-    
+
     user: {
         type: mongoose.Schema.Types.ObjectID,          //connects post to user
         ref: 'user'
     },
 
+    title: {
+        type: String,
+        ref:'user',
+        
+        
+    },
     text: {
         type: String,
         required: true
+
     },
 
     name: {             //name of the user so we can keep post if the account is deleted
         type: String
     },
-    avatar:{
+    avatar: {
         type: String
     },
     
-  
-            upvote: [
-                {
-                    user: {
-                        type: mongoose.Schema.Types.ObjectID,            // user can only vote up or down on a post once. Number cannot increase
-                        ref: 'user'
-                    }
-                }
-            ],
-            downvote: [
-                {
-                    user: {
-                        type: mongoose.Schema.Types.ObjectID,            // user can only vote up or down on a post once. Number cannot increase
-                        ref: 'user'
-                    }
-                }
-            ],totalvotes:[
-            {
-               
-                    type: Number,            // user can only vote up or down on a post once. Number cannot increase
-                    required: true
-                
-            },
-        ],
-        
 
-            
-
-      
-    answer: [             
+    upvote: [
         {
             user: {
-                type: mongoose.Schema.Types.ObjectID,         
+                type: mongoose.Schema.Types.ObjectID,            // user can only vote up or down on a post once. Number cannot increase
                 ref: 'user'
-            } ,
+            }
+        }
+    ],
+    downvote: [
+        {
+            user: {
+                type: mongoose.Schema.Types.ObjectID,            // user can only vote up or down on a post once. Number cannot increase
+                ref: 'user'
+            }
+        }
+    ], totalvotes: [
+        {
+
+            type: Number,            // user can only vote up or down on a post once. Number cannot increase
+            required: true
+
+        },
+    ],
+
+
+
+
+
+    answer: [
+        {
+            user: {
+                type: mongoose.Schema.Types.ObjectID,
+                ref: 'user'
+            },
             text: {
                 type: String,
                 required: true
@@ -70,25 +77,25 @@ const FeedSchema = new mongoose.Schema({
                 type: Date,
                 default: Date.now
             },
-            totalvotesANSW:[
+            totalvotesANSW: [
                 {
-                   
-                        type: Number,            // user can only vote up or down on a post once. Number cannot increase
-                        required: true
-                    
+
+                    type: Number,            // user can only vote up or down on a post once. Number cannot increase
+                    required: true
+
                 },
             ],
-            commentANSW: [               
+            commentANSW: [
                 {
-                  
+
                     user: {
-                        type: mongoose.Schema.Types.ObjectID,         
+                        type: mongoose.Schema.Types.ObjectID,
                         ref: 'user'
-                    } ,
+                    },
                     text: {
                         type: String,
                         required: true
-        
+
                     },
                     avatar: {
                         type: String
@@ -97,80 +104,80 @@ const FeedSchema = new mongoose.Schema({
                         type: Date,
                         default: Date.now
                     },
-                        comANSWvote: [
+                    comANSWvote: [
                         {
                             user: {
                                 type: mongoose.Schema.Types.ObjectID,            // user can only vote up or down on a post once. Number cannot increase
                                 ref: 'user'
                             }
                         }
-                                    ],
-                                    totalvotescomANSW:[
-                                        {
-                                           
-                                                type: Number,            // user can only vote up or down on a post once. Number cannot increase
-                                                required: true
-                                            
-                                        },
-                                    ],
+                    ],
+                    totalvotescomANSW: [
+                        {
+
+                            type: Number,            // user can only vote up or down on a post once. Number cannot increase
+                            required: true
+
+                        },
+                    ],
                 }
-                
+
             ],
 
-           
 
-        
-       
-                 upvoteANS: [
-                    {
-                        user: {
-                            type: mongoose.Schema.Types.ObjectID,            // user can only vote up or down on a post once. Number cannot increase
-                            ref: 'user'
-                        }
-                    }
-                ],
-                downvoteANS: [
-                    {
-                        user: {
-                            type: mongoose.Schema.Types.ObjectID,            // user can only vote up or down on a post once. Number cannot increase
-                            ref: 'user'
-                        }
-                    }
-                ],
-                totalvotesANS:[
-                    {
-                        type: Number,            // user can only vote up or down on a post once. Number cannot increase
-                        required: true
-                    }
-                ],
-            
-            
-    
-        supervote: [
 
-            {
-                user: {
-                    type: mongoose.Schema.Types.ObjectID,
-                    ref: 'user'
+
+
+            upvoteANS: [
+                {
+                    user: {
+                        type: mongoose.Schema.Types.ObjectID,            // user can only vote up or down on a post once. Number cannot increase
+                        ref: 'user'
+                    }
                 }
-    
-            }
-    
-    
-    
-        ],
-    
-    
-    
-    
-    }
-    ],  
-    comment: [               
+            ],
+            downvoteANS: [
+                {
+                    user: {
+                        type: mongoose.Schema.Types.ObjectID,            // user can only vote up or down on a post once. Number cannot increase
+                        ref: 'user'
+                    }
+                }
+            ],
+            totalvotesANS: [
+                {
+                    type: Number,            // user can only vote up or down on a post once. Number cannot increase
+                    required: true
+                }
+            ],
+
+
+
+            supervote: [
+
+                {
+                    user: {
+                        type: mongoose.Schema.Types.ObjectID,
+                        ref: 'user'
+                    }
+
+                }
+
+
+
+            ],
+
+
+
+
+        }
+    ],
+    comment: [
         {
             user: {
-                type: mongoose.Schema.Types.ObjectID,         
+                type: mongoose.Schema.Types.ObjectID,
                 ref: 'user'
-            } ,
+            },
             text: {
                 type: String,
                 required: true
@@ -191,13 +198,13 @@ const FeedSchema = new mongoose.Schema({
                     }
                 }
             ],
-            totalvotesCOM:[
+            totalvotesCOM: [
                 {
                     type: Number,            // user can only vote up or down on a post once. Number cannot increase
-                        required: true
+                    required: true
                 }
-            
-                     ],
+
+            ],
         }
     ],
 
@@ -207,7 +214,6 @@ const FeedSchema = new mongoose.Schema({
     }
 
 
-
 });
 
-module.exports = Feed = mongoose.model('feed',FeedSchema );
+module.exports = Feed = mongoose.model('feed', FeedSchema);
