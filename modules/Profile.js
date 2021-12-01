e9dc6a90ebcb415b736d9245e722837c34035653
RGlovesyou <68345835+SimplyRed900@users.noreply.github.com>
2021-12-01 02:42:07 -0500

    Get User Posts by User ID
    Can get all user posts based on user id. If we want one specific post we will nee to isolate the post id as well.

--- modules/Profile.js
+++ modules/Profile.js
@@ -1,46 +1,49 @@
 const mongoose = require('mongoose');
 
 const ProfileSchema = new mongoose.Schema({
-    user: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'user'
-    },
-    bio: {
-        type: String,
-        required: true
-    },
-    githubname: {
-        type: String
-
-    },
-
-    University: {
-        type: String,
-        required: true
-    },
-    Field: {
-        type: String,
-        required: true
-    },
-
-
-
-    Posts: [
-        {
-
-            user: {
-                type: mongoose.Schema.Types.ObjectID,          //connects post to user
-                ref: 'user'
-            },
-
-            text: {
-                type: String,
-                required: true
-            },
-            
-
-        }
-    ],
+   
+        user: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'user'
+        },
+        bio: {
+            type: String,
+            required: true
+        },
+        githubname: {
+            type: String
+    
+        },
+    
+        University: {
+            type: String,
+            required: true
+        },
+        Field: {
+            type: String,
+            required: true
+        },
+    
+    
+    
+        Posts: [
+            {
+                    type: mongoose.Schema.Types.ObjectId,
+                    ref:'Feed',
+                    
+                    
+                }
+    
+        ],
+        Titles:[
+            {
+                type: mongoose.Schema.Types.ObjectId,
+                ref:'Feed',
+                
+                
+            }
+        ]
+    
 });
 
 module.exports = Profile = mongoose.model('profile', ProfileSchema);
