e839646ecd7461fa4e37044859e7e9b70d064f90
Lyuba Georgieva <lyubageorgieva@hotmail.com>
2021-12-27 22:35:56 -0500

    Fixing the redirect link after a user posts a question to be directed to feed #137

--- client/src/components/feed/PostForm.js
+++ client/src/components/feed/PostForm.js
@@ -3,11 +3,13 @@ import PropTypes from 'prop-types';
 import { connect }  from 'react-redux';
 import { addPost } from '../../actions/post';
 import { Link } from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
 
 
 const PostForm = ({ addPost }) => {
     const [title, setTitle] = useState('');
     const [text, setText] = useState('');
+    const history = useHistory();
 
     return (
         <section className="question-section">
@@ -27,8 +29,10 @@ const PostForm = ({ addPost }) => {
                     <p>Include as much information as you can.</p>
                     <textarea name="post-body" id="post-body-details" cols="125" rows="10" value={text} onChange={e => setText(e.target.value)} required></textarea>
                 </div>
-                <button id="post-form-button" className="submit" type="submit">Submit Question</button>
+                <button id="post-form-button" className="submit" type="submit" onClick={() => {history.goBack()}}>Submit Question</button>
+                <div>
                 <h4 className="have-account">Wanna go back?<Link id="linkAuth" to="/feed">{" "}Q&A</Link></h4>
+                </div>
             </form>
             
         </section>
